890b83debab4a98a8a353e652ef95342192324de
Colin Putney <colin.putney@descarteslabs.com>
2022-01-04 14:32:58 -0700

    Add a facade for simpler instrumentation

--- jot/__init__.py
+++ jot/__init__.py
@@ -1,1 +1,73 @@
 from .telemeter import Telemeter
+from contextlib import contextmanager as _contextmanager
+
+active = Telemeter()
+_stack = []
+
+
+def _push(telemeter):
+    global active
+    _stack.append(active)
+    active = telemeter
+
+
+def _pop():
+    global active
+    active = _stack.pop()
+
+
+def init(target, tags):
+    global active, _stack
+    active = Telemeter(target, None, tags)
+    _stack = []
+
+
+def start(*args, **kwargs):
+    child = active.start(*args, **kwargs)
+    _push(child)
+
+
+def finish(*args, **kwargs):
+    active.finish(*args, **kwargs)
+    _pop()
+
+
+def event(name, *tagdicts):
+    return active.event(name, *tagdicts)
+
+
+def debug(message, *tagdicts):
+    return active.debug(message, *tagdicts)
+
+
+def info(message, *tagdicts):
+    return active.info(message, *tagdicts)
+
+
+def warning(message, *tagdicts):
+    return active.warning(message, *tagdicts)
+
+
+def error(message, exc, *tagdicts):
+    return active.error(message, exc, *tagdicts)
+
+
+def magnitude(name, value, *tagdicts):
+    return active.magnitude(name, value, *tagdicts)
+
+
+def count(name, value, *tagdicts):
+    return active.count(name, value, *tagdicts)
+
+
+@_contextmanager
+def span(name, *tagdicts):
+    child = active.start(name, *tagdicts)
+    _push(child)
+    try:
+        yield child
+    except Exception as exc:
+        child.error(f"Error during {name}", exc)
+    finally:
+        child.finish()
+        _pop()
---
+++ tests/test_facade.py
@@ -0,0 +1,103 @@
+import jot
+import pytest
+from jot.base import Span, Target
+from jot.telemeter import Telemeter
+
+LOOZY = {"loozy": 34}
+
+
+@pytest.fixture
+def assert_forwards(mocker):
+    def _assert_forwards(method_name, *args):
+        # spy on the method
+        spy = mocker.spy(jot.active, method_name)
+
+        # call the method
+        func = getattr(jot, method_name)
+        func(*args)
+
+        # assert the call was forwarded
+        spy.assert_called_once_with(*args)
+
+    return _assert_forwards
+
+
+def test_active():
+    assert isinstance(jot.active, Telemeter)
+
+
+def test_init():
+    target = Target()
+    jot.init(target, LOOZY)
+    assert jot.active.target is target
+    assert jot.active.tags == LOOZY
+
+
+def test_start():
+    parent = jot.active
+    jot.start("child", {"nork": 91})
+
+    assert jot.active is not parent
+    assert jot.active.tags["nork"] == 91
+    assert jot.active.span.parent_id == parent.span.id
+
+
+def test_finish():
+    parent = jot.active
+    jot.start("child")
+    jot.finish()
+
+    assert jot.active is parent
+
+
+def test_with():
+    parent = jot.active
+    with jot.span("child", {"lep": 66}) as child:
+        assert child is jot.active
+        assert child is not parent
+        assert child.span.parent_id == parent.span.id
+        assert child.tags["lep"] == 66
+    assert jot.active is parent
+
+
+def test_with_error(mocker):
+    spy = mocker.spy(jot.active.target, "error")
+    with jot.span("child", {"nork": 6}):
+        1 / 0
+    spy.assert_called_once()
+    assert isinstance(spy.call_args.args[0], Span)
+    assert spy.call_args.args[0].parent_id == jot.active.span.id
+    assert spy.call_args.args[1] == "Error during child"
+    assert isinstance(spy.call_args.args[2], ZeroDivisionError)
+    assert spy.call_args.args[3]["nork"] == 6
+
+
+def test_event(assert_forwards):
+    assert_forwards("event", "name", LOOZY)
+
+
+def test_debug(assert_forwards):
+    assert_forwards("debug", "debug message", LOOZY)
+
+
+def test_info(assert_forwards):
+    assert_forwards("info", "info message", LOOZY)
+
+
+def test_warning(assert_forwards):
+    assert_forwards("warning", "warning message", LOOZY)
+
+
+def test_error(assert_forwards):
+    try:
+        1 / 0
+    except ZeroDivisionError as exc:
+        assert_forwards("error", "error message", exc, LOOZY)
+
+
+def test_magnitude(assert_forwards):
+    assert_forwards("magnitude", "temperature", 99.0, LOOZY)
+
+
+def test_count(assert_forwards):
+    assert_forwards("count", "requests", 99, LOOZY)
